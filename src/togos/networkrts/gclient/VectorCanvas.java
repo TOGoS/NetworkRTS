03c9a961a092b9c884a08923d8e82532d9f43b69
TOGoS <togos00@gmail.com>
2012-04-01 15:01:41 -0500

    Some very minor progress on double-buffering and abstracting what is to be drawn in VectorCanvas.

--- src/togos/networkrts/gclient/VectorCanvas.java
+++ src/togos/networkrts/gclient/VectorCanvas.java
@@ -5,82 +5,125 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Frame;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.util.Iterator;
+import java.awt.image.BufferedImage;
 
 import togos.networkrts.simulation.Simulator;
-import togos.networkrts.world.PositionFunction;
-import togos.networkrts.world.WorldObject;
 
 public class VectorCanvas extends Canvas
 {
 	private static final long serialVersionUID = 1L;
 	
 	long cwx = 0, cwy = 0;
-	double zoom = 1.0, rotation = 0.0;
+	double zoom = 10.0/1024, rotation = 1.0;
 	
 	Simulator sim = new Simulator();
 	
 	public VectorCanvas() {
 	}
 	
-	protected long curTime() {
-		return System.currentTimeMillis();
-	}
+	AWTPainter painter = new TestAWTPainter();
 	
-	public void paint( WorldObject o, long ts, Graphics g, double x, double y, double scale ) {
-		g.setColor( o.color );
-		g.fillRect( (int)x, (int)y, (int)(o.rad*2*scale), (int)(o.rad*2*scale) );
-	}
+	volatile BufferedImage buffer;
 	
-	public void paint( double cwx, double cwy, Graphics g, double scale ) {
-		int csx = getWidth()/2;
-		int csy = getHeight()/2;
-		long ts = curTime();
-		for( Iterator i=sim.getAllObjects().iterator(); i.hasNext(); ) {
-			WorldObject o = (WorldObject)i.next();
-			PositionFunction pf = o.pos;
-			paint( o, curTime(), g, (pf.getX(ts) - cwx)*scale + csx, (pf.getY(ts) - cwy)*scale + csy, scale );
+	class Repainter extends Thread {
+		long repaintInterval;
+		
+		public Repainter( long repaintInterval ) {
+			this.repaintInterval = repaintInterval;
 		}
+		
+		protected BufferedImage getBuffer() {
+			if( buffer == null || buffer.getWidth() != getWidth() || buffer.getHeight() != getHeight() ) {
+				buffer = getGraphicsConfiguration().createCompatibleImage( getWidth(), getHeight() );
+			}
+			return buffer;
+		}
+		
+		public void run() {
+			while( !Thread.interrupted() ) { 
+				BufferedImage buffer = getBuffer();
+				Graphics g = buffer.getGraphics();
+				g.setClip( 0, 0, buffer.getWidth(), buffer.getHeight() );
+				paintBuffer( g );
+				repaint();
+				try {
+					Thread.sleep(repaintInterval);
+				} catch( InterruptedException e ) {
+					Thread.currentThread().interrupt();
+					return;
+				}
+			}
+		};
+	};
+	
+	Repainter repainter;
+	
+	public void paintBuffer( Graphics g ) {
+		Graphics2D g2d = (Graphics2D)g;
+		
+		Rectangle clip = g.getClipBounds();
+		
+		g.setColor( Color.BLACK );
+		g.fillRect( clip.x, clip.y, clip.width, clip.height );
+		
+		g2d.translate( getWidth() / 2, getHeight() / 2 );
+		g2d.scale( zoom, zoom );
+		g2d.rotate( rotation );
+		
+		painter.paint( g2d, System.currentTimeMillis() );
 	}
 	
 	public void paint( Graphics g ) {
-		paint( cwx, cwx, g, zoom );
+		if( repainter == null ) {
+			repainter = new Repainter(20);
+			repainter.start();
+		}
+		BufferedImage img = buffer;
+		if( img == null ) {
+			g.setColor(Color.DARK_GRAY);
+			g.fillRect(0, 0, getWidth(), getHeight());
+			g.setColor(Color.LIGHT_GRAY);
+			g.drawString("No backing buffer", 12, 12 );
+		} else {
+			g.drawImage( img, (getWidth() - img.getWidth()) / 2, (getHeight() - img.getHeight()) / 2, null );
+		}
 	}
 	
+	/* I still see background color when I resize the window.
+	 * I think the parent component is drawing it, because overriding update
+	 * does prevent it from filling the entire background each time. */
+	public void update( Graphics g ) {
+		paint(g);
+	}
+	
+	public void setVisible( boolean vis ) {
+		super.setVisible(vis);
+		if( vis ) {
+
+		} else {
+			if( repainter != null ) {
+				repainter.interrupt();
+				repainter = null;
+			}
+		}
+	}
 	
 	public static void main( String[] args ) {
-		Frame f = new Frame("NetworkRTS");
+		Frame f = new Frame("NetworkRTS VectorCanvas test");
 		
 		final VectorCanvas c = new VectorCanvas();
 		c.setPreferredSize(new Dimension(640,480));
-		c.setBackground(Color.BLACK);
-		
-		final Thread t = new Thread() {
-			public void run() {
-				long t = System.currentTimeMillis(); 
-				try {
-					while( !Thread.interrupted() ) {
-						long destTime = t+100;
-						c.sim.runUntil(destTime);
-						c.repaint();
-						long sleepTime = destTime - System.currentTimeMillis();
-						if( sleepTime > 0 ) Thread.sleep( sleepTime );
-						t = destTime;
-					}
-				} catch( InterruptedException e ) {
-				}
-			};
-		};
-		t.start();
+		c.setBackground(Color.ORANGE);
 		
 		f.add(c);
 		f.pack();
 		f.addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent e) {
 				e.getWindow().dispose();
-				t.interrupt();
 			}
 		});
 		f.setVisible(true);
