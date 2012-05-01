9098707da35397d0205bd3282b7ea4821414bb83
TOGoS <togos00@gmail.com>
2012-05-01 18:16:27 -0500

    Add ICMPResponder and required util classes.

---
+++ src/togos/networkrts/experimental/netsim1/ICMPResponder.java
@@ -0,0 +1,153 @@
+package togos.networkrts.experimental.netsim1;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketAddress;
+
+import togos.blob.ByteChunk;
+import togos.blob.SimpleByteChunk;
+import togos.networkrts.inet.PacketUtil;
+import togos.networkrts.util.ByteUtil;
+
+/**
+ * Demonstrates parsing IPv6 headers and ICMP echo requests and responding to
+ * them. IP packets must be encapsulated in UDP packets and sent to the port
+ * that ICMPResponder listens on, e.g. by TUN2UDP -tun -no-pi
+ */
+public class ICMPResponder
+{
+	interface PacketIO {
+		public ByteChunk recv();
+		public void send(ByteChunk c);
+	}
+	
+	static class DatagramPacketIO implements PacketIO {
+		public static final int DEFAULT_MTU = 2048;
+		
+		DatagramSocket sock;
+		int mtu;
+		SocketAddress lastReceivedFrom;
+		
+		public DatagramPacketIO( DatagramSocket sock, int mtu ) {
+			this.sock = sock;
+			this.mtu = mtu;
+		}
+		public DatagramPacketIO( DatagramSocket sock ) {
+			this( sock, DEFAULT_MTU );
+		}
+		
+		public ByteChunk recv() {
+			DatagramPacket p = new DatagramPacket(new byte[mtu], mtu);
+			try {
+				sock.receive(p);
+				lastReceivedFrom = p.getSocketAddress();
+			} catch( IOException e ) {
+				throw new RuntimeException(e);
+			}
+			return new SimpleByteChunk(p.getData(), p.getOffset(), p.getLength());
+		}
+		
+		public void send(ByteChunk c) {
+			// If we don't know where to send it, just drop it.
+			if( lastReceivedFrom == null ) return;
+			
+			DatagramPacket p = new DatagramPacket(c.getBuffer(), c.getOffset(), c.getSize());
+			p.setSocketAddress(lastReceivedFrom);
+			try {
+				sock.send(p);
+			} catch( IOException e ) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
+	
+	static class IPPacketIOWrapper implements PacketIO {
+		PacketIO pio;
+		
+		public IPPacketIOWrapper( PacketIO pio ) {
+			this.pio = pio;
+		}
+		
+		public ByteChunk recv() {
+			while( true ) {
+				ByteChunk c = pio.recv();
+				
+				System.err.print( "Received packet: " );
+				PacketUtil.dumpPacket(c.getBuffer(), c.getOffset(), c.getSize(), System.err);
+				
+				if( c != null ) return c;
+			}
+		}
+		
+		public void send(ByteChunk c) {
+			System.err.print( "Sending packet: " );
+			PacketUtil.dumpPacket(c.getBuffer(), c.getOffset(), c.getSize(), System.err);
+			pio.send(c);
+		}
+	}
+	
+	static class IPPacketHandler {
+		protected final PacketIO responder;
+		public IPPacketHandler( PacketIO responder ) {
+			this.responder = responder;
+		}
+		
+		protected ByteChunk createIcmpEchoResponse( byte[] ping, int offset, int size ) {
+			byte[] pong = new byte[size];
+			
+			ByteUtil.copy( ping, offset, pong, 0, size );
+
+			int trafficClass = 0;
+			int flowLabel = 0;
+			ByteUtil.encodeInt32( (6 << 28) | (trafficClass << 20) | flowLabel, pong, 0 );
+			ByteUtil.copy( ping, offset+8, pong, offset+24, 16 ); // Source address
+			ByteUtil.copy( ping, offset+24, pong, offset+8, 16 ); // Dest address
+			final int payloadOffset = PacketUtil.IP6_HEADER_SIZE;
+			pong[payloadOffset + 0] = (byte)129; // ICMPv6 echo response 
+			pong[payloadOffset + 1] =         0; // ICMPv6 code
+			pong[payloadOffset + 2] =         0; // Cleared checksum
+			pong[payloadOffset + 3] =         0; // Cleared checksum
+			// Then calculate actual checksum
+			int checksum = (int)PacketUtil.calculateIcmp6Checksum(pong, offset, size);
+			ByteUtil.encodeInt16( checksum, pong, payloadOffset + 2 );
+			return new SimpleByteChunk(pong);
+		}
+		
+		protected void handleIcmp6Packet( byte[] packet, int offset, int size ) {
+			switch( packet[offset+PacketUtil.IP6_HEADER_SIZE] & 0xFF ) {
+			case( 128 ): // Echo request
+				responder.send( createIcmpEchoResponse(packet, offset, size) );
+				break;
+			}
+		}
+		
+		public void handleIp6( byte[] packet, int offset, int size ) {
+			switch( PacketUtil.getIp6ProtocolNumber(packet, offset) ) {
+			case( 58 ): // ICMPv6
+				handleIcmp6Packet( packet, offset, size ); break;
+			}
+		}
+		
+		public void handle( byte[] packet, int offset, int size ) {
+			if( size < PacketUtil.IP4_HEADER_SIZE ) {
+				throw new IndexOutOfBoundsException("Packet smaller than IP4 header!");
+			}
+			switch( (packet[offset] >> 4) & 0xF ) {
+			case( 4 ): break;
+			case( 6 ): handleIp6( packet, offset, size ); break;
+			}
+		}
+	}
+	
+	public static void main( String[] args ) throws Exception {
+		DatagramSocket s = new DatagramSocket(7777);
+		DatagramPacketIO io = new DatagramPacketIO(s);
+		IPPacketIOWrapper ipio = new IPPacketIOWrapper(io);
+		IPPacketHandler h = new IPPacketHandler(ipio);
+		ByteChunk packet;
+		while( (packet = ipio.recv()) != null ) {
+			h.handle(packet.getBuffer(), packet.getOffset(), packet.getSize());
+		}
+	}
+}
---
+++ src/togos/networkrts/inet/PacketUtil.java
@@ -0,0 +1,88 @@
+package togos.networkrts.inet;
+
+import java.io.PrintStream;
+
+import togos.networkrts.util.ByteUtil;
+
+public class PacketUtil {
+	
+	// Based on
+	// http://support.novell.com/techcenter/articles/img/nc1999_0502.gif
+	// http://routemyworld.com/wp-content/uploads/2009/01/ipv6header.png
+	
+	public static final int IP4_HEADER_SIZE = 20;
+	public static final int IP6_HEADER_SIZE = 40;
+	public static final int ICMP_HEADER_SIZE = 8;
+	
+	public static int getIp6PayloadLength( byte[] packet, int packetOffset ) {
+		return ByteUtil.decodeInt16( packet, packetOffset + 4 );
+	}
+	
+	public static int getValidatedIp6PayloadLength( byte[] packet, int packetOffset, int packetSize ) {
+		int len = getIp6PayloadLength(packet, packetOffset);
+		if( len < 0 ) throw new IndexOutOfBoundsException("Packet's payload length is < 0: "+len);
+		if( len + 40 > packetSize ) throw new IndexOutOfBoundsException("Packet's payload length is too large for packet of size "+packetSize+": "+len);
+		return len;
+	}
+	
+	public static int getIpVersion( byte[] packet, int packetOffset, int packetSize ) {
+		return (packet[packetOffset] >> 4) & 0xF;
+	}
+	
+	public static int getIp6TrafficClass( byte[] packet, int packetOffset ) {
+		return (ByteUtil.decodeInt32( packet, packetOffset+0) >> 20) & 0xFF;
+	}
+	
+	public static int getIp6ProtocolNumber( byte[] packet, int packetOffset ) {
+		return packet[packetOffset+6] & 0xFF;
+	}
+	
+	public static int getIp6PayloadOffset( int packetOffset ) {
+		return packetOffset + 40;
+	}
+	
+	public static long calculateIcmp6Checksum( byte[] packet, int offset, int size ) {
+		int payloadLength = getValidatedIp6PayloadLength( packet, offset, size );
+		
+		byte[] data = new byte[40+payloadLength];
+		ByteUtil.copy( packet, offset+8,  data,  0, 16 ); // Source address
+		ByteUtil.copy( packet, offset+24, data, 16, 16 ); // Destination address
+		ByteUtil.encodeInt32(             payloadLength, data, 32 );
+		ByteUtil.encodeInt32( getIp6ProtocolNumber( packet, offset ), data, 36 );
+		ByteUtil.copy( packet, getIp6PayloadOffset(offset), data, 40, payloadLength );
+		return InternetChecksum.checksum( data );
+	}
+	
+	protected static void dumpIcmp6Data( byte[] icmpMessage, int offset, int size, PrintStream ps ) {
+		ByteUtil.ensureRoom( size, 0, ICMP_HEADER_SIZE, "ICMP header" );
+		
+		ps.println( "    ICMP message type: "+(icmpMessage[offset]&0xFF) );
+		ps.println( "    ICMP code: "+(icmpMessage[offset+1]&0xFF) );
+		ps.println( "    ICMP checksum: "+ByteUtil.decodeInt16( icmpMessage, offset+2 ) );
+	}
+	
+	protected static void dumpIp6Packet( byte[] packet, int offset, int size, PrintStream ps ) {
+		ByteUtil.ensureRoom( size, 0, IP6_HEADER_SIZE, "IP6 header" );
+		
+		ps.println("IPv6 packet");
+		ps.println("  from: "+AddressUtil.formatIp6Address(packet, 8));
+		ps.println("  to:   "+AddressUtil.formatIp6Address(packet, 24));
+		ps.println("  hoplimit: "+(packet[offset+7] & 0xFF));
+		ps.println("  payload length: " + ByteUtil.decodeInt16(packet, offset+4) );
+		ps.println("  traffic class: " + getIp6TrafficClass(packet, offset) );
+		ps.println("  protocol number: " + getIp6ProtocolNumber(packet, offset) );
+		
+		switch( getIp6ProtocolNumber(packet, offset) ) {
+		case( 58 ):
+			dumpIcmp6Data( packet, getIp6PayloadOffset(offset), getValidatedIp6PayloadLength(packet,offset,size), ps );
+		}
+		
+		ps.println( "    calculated ICMPv6 checksum: "+calculateIcmp6Checksum(packet, offset, size) );
+	}
+	
+	public static void dumpPacket( byte[] packet, int offset, int length, PrintStream ps ) {
+		switch( (packet[offset+0] >> 4) & 0xF ) {
+		case( 6 ): dumpIp6Packet( packet, offset, length, ps ); 
+		}
+	}
+}
