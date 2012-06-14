683ba0a537264208e8b0d03707ff2a3cd6513898
TOGoS <togos00@gmail.com>
2012-06-14 15:50:20 -0500

    Improve RectIntersector documentation.

--- src/togos/networkrts/experimental/shape/RectIntersector.java
+++ src/togos/networkrts/experimental/shape/RectIntersector.java
@@ -6,6 +6,17 @@ public interface RectIntersector
 	public static final int INCLUDES_SOME = 1;
 	public static final int INCLUDES_ALL  = 2;
 	
-	/** Does this shape contain none, some, or all of the given rectangle? */ 
+	/**
+	 * Returns one of the INCLUDE_ values depending on how much area
+	 * of the given rectangle overlaps this shape.  Edges touching
+	 * does not count as overlap!
+	 * 
+	 * Behavior is undefined if width or height are zero or negative.
+	 * 
+	 * @return
+	 *   INCLUDE_NONE if the given rectangle is entirely outside of this shape,
+	 *   INCLUDE_SOME if the given rectangle is partly inside of this shape, or
+	 *   INCLUDE_ALL if the given rectangle is entirely inside of this shape
+	 */ 
 	public int rectIntersection( double x, double y, double w, double h );
 }
