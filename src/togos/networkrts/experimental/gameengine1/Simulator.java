c7304fe93065452928b18c746c85905585bb8ba3
TOGoS <togos00@gmail.com>
2013-03-05 14:06:19 -0600

    Completely refactor gameengine1.Simulator to use immutable objects because they're so much easier to deal with.

--- src/togos/networkrts/experimental/gameengine1/Simulator.java
+++ src/togos/networkrts/experimental/gameengine1/Simulator.java
@@ -11,42 +11,113 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
 import java.util.HashSet;
-import java.util.Iterator;
 
 import togos.networkrts.experimental.gensim.BaseMutableAutoUpdatable;
 import togos.networkrts.experimental.gensim.EventLoop;
 import togos.networkrts.experimental.gensim.QueuelessRealTimeEventSource;
-import togos.networkrts.experimental.gensim.RealTimeEventSource;
 
 public class Simulator extends BaseMutableAutoUpdatable<Object>
 {
+	static class Entity
+	{
+		public final Object tag;
+		public final Entity parent;
+		public final long time;
+		public final double x, y, z, spaceRadius, solidRadius;
+		public final double vx, vy, vz;
+		public final double ax, ay, az;
+		public final double mass;
+		public final Color color;
+		public final EntityBehavior behavior;
+		
+		public final AABB boundingBox;
+		public final boolean isMoving;
+		public final boolean isSolid;
+		
+		public Entity(
+			Object tag, Entity parent, long time,
+			double x, double y, double z,
+			double vx, double vy, double vz,
+			double ax, double ay, double az,
+			double spaceRadius, double solidRadius,
+			double mass, Color color, EntityBehavior behavior
+		) {
+			this.tag = tag;
+			this.parent = parent;
+			this.time = time;
+			this.x = x; this.y = y; this.z = z;
+			this.vx = vx; this.vy = vy; this.vz = vz;
+			this.ax = ax; this.ay = ay; this.az = az;
+			this.spaceRadius = spaceRadius; this.solidRadius = solidRadius;
+			this.mass = mass; this.color = color;
+			this.behavior = behavior;
+			this.boundingBox = new AABB(
+				x-solidRadius, y-solidRadius, z-solidRadius,
+				x+solidRadius, y+solidRadius, z+solidRadius
+			);
+			this.isMoving = vx != 0 || vy != 0 || vz != 0 || ax != 0 || ay != 0 || az != 0;
+			this.isSolid = solidRadius > 0;
+		}
+		
+		public Entity withUpdatedPosition( long targetTime ) {
+			final double duration = (targetTime-time)/1000.0;
+			final double halfDurationSquared = duration*duration/2;
+			return withPosition(
+				targetTime,
+				x + vx*duration + ax*halfDurationSquared,
+				y + vy*duration + ay*halfDurationSquared,
+				z + vz*duration + az*halfDurationSquared,
+				vx + ax*duration,
+				vy + ay*duration,
+				vz + az*duration,
+				ax, ay, az
+			);
+		}
+		
+		public Entity withPosition( long time,
+			double x, double y, double z,
+			double vx, double vy, double vz,
+			double ax, double ay, double az
+		) {
+			return new Entity(
+				tag, parent, time,
+				x, y, z, vx, vy, vz, ax, ay, az,
+				spaceRadius, solidRadius,
+				mass, color, behavior
+			);
+		}
+	}
+	
+	interface EntityShell
+	{
+		public void add( Entity e );
+	}
+	
+	interface EntityUpdater
+	{
+		public Entity update( Entity e, EntityShell shell );
+	}
+	
 	interface EntityBehavior
 	{
-		public void onMove( long time, Entity self );
-		public void onCollision( long time, Entity self, Entity other );
+		public Entity onMove( long time, Entity self, EntityShell shell );
+		public Entity onCollision( long time, Entity self, Entity other, EntityShell shell );
 	}
 	
 	/** Axis-aligned bounding box */
-	static class AABB<PayloadClass> {
-		double minX, minY, minZ, maxX, maxY, maxZ;
-		public PayloadClass payload;
+	static class AABB {
+		final double minX, minY, minZ, maxX, maxY, maxZ;
 		
-		public AABB( double x0, double y0, double z0, double x1, double y1, double z1, PayloadClass payload ) {
+		public AABB( double x0, double y0, double z0, double x1, double y1, double z1 ) {
 			minX = x0;  maxX = x1;
 			minY = y0;  maxY = y1;
 			minZ = z0;  maxZ = z1;
-			this.payload = payload;
 		}
-		public AABB( PayloadClass payload ) {
-			this.payload = payload;
-		}
-		public AABB() {}
 		
-		public final void setCentered( double x, double y, double z, double rad ) {
-			minX = x - rad;  maxX = x + rad;
-			minY = y - rad;  maxY = y + rad;
-			minZ = z - rad;  maxZ = z + rad;
+		public final AABB centered( double x, double y, double z, double rad ) {
+			return new AABB( x-rad, y-rad, z-rad, x+rad, y+rad, z+rad );
 		}
+		
 		public final boolean intersects( AABB other ) {
 			if( maxX < other.minX ) return false;
 			if( maxY < other.minY ) return false;
@@ -62,200 +133,180 @@ public class Simulator extends BaseMutableAutoUpdatable<Object>
 		public void visit( Type v );
 	}
 	
-	interface SpatialIndex<PayloadClass> {
-		public void add( AABB<PayloadClass> box );
-		public void remove( AABB<PayloadClass> box );
-		public void findIntersection( AABB<?> bounds, Visitor<AABB<PayloadClass>> callback );
+	interface SpatialIndex<Item> {
+		public void forEachItemIntersecting( AABB bounds, Visitor<Item> callback );
+	}	
+	
+	protected static final boolean solidCollision( Entity e1, Entity e2 ) {
+		assert e1 != e2;
+		
+		double dx = e1.x-e2.x, dy = e1.y-e2.y, dz = e1.z-e2.z;
+		double distSquared = dx*dx + dy*dy + dz*dz;
+		double radSum = e1.solidRadius + e2.solidRadius;
+		return distSquared < radSum*radSum;
 	}
 	
-	static class DumbSpatialIndex<PayloadClass> implements SpatialIndex<PayloadClass>
+	static class EntityIndex implements SpatialIndex<Entity>
 	{
-		HashSet<AABB<PayloadClass>> boxes = new HashSet<AABB<PayloadClass>>();
+		private final HashSet<Entity> allEntities = new HashSet<Entity>();
+		private int movingEntityCount = 0;
 		
-		@Override public void add(AABB box) {
-			boxes.add(box);
+		public boolean hasMovingEntities() {
+			return movingEntityCount > 0;
 		}
-		@Override public void remove(AABB box) {
-			boxes.remove(box);
+		
+		public void add(Entity e) {
+			allEntities.add(e);
+			if( e.isMoving ) ++movingEntityCount;
 		}
-		@Override public void findIntersection(AABB<?> bounds, Visitor<AABB<PayloadClass>> callback) {
-			for( AABB box : boxes ) {
-				if( box.intersects(bounds) ) callback.visit(box);
+		
+		@Override public void forEachItemIntersecting(AABB bounds, Visitor<Entity> callback) {
+			for( Entity e : allEntities ) {
+				if( e.boundingBox.intersects(bounds) ) callback.visit(e);
 			}
 		}
-	}
-	
-	static class Space
-	{
-		final HashSet<Entity> allEntities = new HashSet();
-		final HashSet<Entity> movingEntities = new HashSet();
-		final SpatialIndex<Entity> solidIndex = new DumbSpatialIndex();
 		
-		protected void updateEntity( Entity e ) {
-			if( e.isMoving() ) movingEntities.add(e);
-			else movingEntities.remove(e);
-		}
-		
-		public void addEntity( Entity e ) {
-			allEntities.add( e );
-			solidIndex.add( e.updateBoundingBox() );
-			updateEntity( e );
-		}
-		
-		protected boolean solidCollision( Entity e1, Entity e2 ) {
-			double dx = e1.x-e2.x, dy = e1.y-e2.y, dz = e1.z-e2.z;
-			double distSquared = dx*dx + dy*dy + dz*dz;
-			double radSum = e1.solidRadius + e2.solidRadius;
-			return distSquared < radSum*radSum;
-		}
-		
-		long currentTime;
-		
-		Entity collisionCheckEntity;
-		Visitor<AABB<Entity>> collisionCallback = new Visitor<Simulator.AABB<Entity>>() {
-			@Override public void visit(AABB<Entity> v) {
-				if( collisionCheckEntity != v.payload && solidCollision(collisionCheckEntity, v.payload) ) {
-					collisionCheckEntity.behavior.onCollision(currentTime, collisionCheckEntity, v.payload);
-					// If v is moving, this will be called by the outer loop
-					if( !v.payload.isMoving() ) v.payload.behavior.onCollision(currentTime, v.payload, collisionCheckEntity);
+		public EntityIndex updateEntities( EntityUpdater u ) {
+			final EntityIndex newIndex = new EntityIndex();
+			final EntityShell shell = new EntityShell() {
+				@Override public void add( Entity e ) {
+					newIndex.add(e);
+				}
+			};
+			for( Entity e : allEntities ) {
+				if( (e = u.update(e, shell)) != null ) {
+					newIndex.add(e);
 				}
 			}
-		};
-		
-		/**
-		 * Call after changing an entity's coordinates but before updating its bounding box.
-		 */
-		private void updateEntityBounds(Entity e) {
-			solidIndex.remove(e.boundingBox);
-			e.updateBoundingBox();
-			solidIndex.add(e.boundingBox);
-		}
-		
-		public void passTime( double duration, long targetTime ) {
-			currentTime = targetTime;
-			for( Entity e : movingEntities ) {
-				e.updatePosition(duration);
-				e.behavior.onMove( currentTime, e );
-				updateEntityBounds( e );
-			}
-			for( Entity e : movingEntities ) {
-				collisionCheckEntity = e;
-				solidIndex.findIntersection( e.boundingBox, collisionCallback );
-			}
-			for( Iterator<Entity> i=movingEntities.iterator(); i.hasNext(); ) {
-				if( !i.next().isMoving() ) {
-					System.err.println("Stopped entity; removing; "+movingEntities.size()+" left");
-					i.remove();
-				}
-			}
+			return newIndex;
 		}
 	}
 	
-	static class Entity
-	{
-		public final AABB boundingBox = new AABB(this);
-		public Entity parent;
-		public double x, y, z, spaceRadius, solidRadius;
-		public double vx, vy, vz;
-		public double ax, ay, az;
-		public double mass;
-		public Color color;
-		public EntityBehavior behavior;
-		
-		protected boolean isMoving() {
-			return vx != 0 || vy != 0 || vz != 0 || ax != 0 || ay != 0 || az != 0;
-		}
-		
-		protected void updatePosition( double duration ) {
-			double halfDurationSquared = duration*duration/2;
-			x += vx*duration + ax*halfDurationSquared;
-			y += vy*duration + ay*halfDurationSquared;
-			z += vz*duration + az*halfDurationSquared;
-			vx += ax*duration;
-			vy += ay*duration;
-			vz += az*duration;
-		}
-		
-		public AABB updateBoundingBox() {
-			double maxRadius = Math.max(spaceRadius, solidRadius);
-			this.boundingBox.setCentered( x, y, z, maxRadius );
-			return boundingBox;
-		}
-	}
-	
-	public long physicsInterval = 80;
-	Space space = new Space();
+	EntityIndex entityIndex = new EntityIndex();
+	public long physicsInterval = 50;
 	HashSet<Runnable> worldUpdateListeners = new HashSet<Runnable>();
 	
 	protected long getNextPhysicsUpdateTime() {
-		return space.movingEntities.size() > 0 ? currentTime + physicsInterval : TIME_INFINITY;
+		return entityIndex.hasMovingEntities() ? currentTime + physicsInterval : TIME_INFINITY;
 	}
 	
 	@Override public long getNextAutomaticUpdateTime() {
 		return Math.min(getNextPhysicsUpdateTime(), super.getNextAutomaticUpdateTime());
 	}
-
+	
 	@Override
-	protected void passTime(long targetTime) {
+	protected void passTime(final long targetTime) {
 		System.err.println( (targetTime - currentTime) + " milliseconds passed to "+targetTime );
-		space.passTime( (targetTime - currentTime)/1000.0, targetTime );
+		System.err.println( "Moving entities: "+entityIndex.movingEntityCount );
+		entityIndex = entityIndex.updateEntities( new EntityUpdater() {
+			protected double damp( double v, double min ) {
+				return Math.abs(v) < min ? 0 : v;
+			}
+			
+			@Override public Entity update( Entity e, EntityShell shell ) {
+				e = e.withUpdatedPosition(targetTime);
+				if( e != null && e.y <= e.solidRadius && e.vy <= 0 ) {
+					if( Math.abs(e.vy) < 20 ) {
+						e = e.withPosition(
+							e.time,
+							e.x, e.solidRadius, e.z,
+							damp(e.vx * 0.9, 0.1), 0, e.vz,
+							e.ax, 0, e.az
+						);
+					} else {
+						e = e.withPosition(
+							e.time,
+							e.x, e.y, e.z,
+							damp(e.vx * 0.9, 0.1), e.vy*-0.9, e.vz,
+							e.ax, e.ay, e.az
+						);
+					}
+				}
+				if( e != null ) {
+					e = e.behavior.onMove( targetTime, e, shell );
+				}
+				return e;
+			}
+		} );
+		entityIndex = entityIndex.updateEntities( new EntityUpdater() {
+			Entity collisionCheckEntity;
+			Entity collisionTargetEntity;
+			Visitor<Entity> collisionChecker = new Visitor<Entity>() {
+				@Override public void visit( Entity v ) {
+					if( v == collisionCheckEntity ) return;
+					
+					if( solidCollision(collisionCheckEntity, v) ) {
+						collisionTargetEntity = v;
+					}
+				}
+			};
+			@Override
+			public Entity update( Entity e, EntityShell shell ) {
+				collisionCheckEntity = e;
+				collisionTargetEntity = null;
+				entityIndex.forEachItemIntersecting( e.boundingBox, collisionChecker );
+				return collisionTargetEntity == null ? e : 
+					e.behavior.onCollision( targetTime, e, collisionTargetEntity, shell );
+			}
+		} );
 		currentTime = targetTime;
 		for( Runnable r : worldUpdateListeners ) r.run();
 	}
 
 	@Override
 	protected void handleEvent(Object evt) {
-		for( Entity e : space.allEntities ) {
-			e.vy += Math.random()*100;
-			e.ay = -100;
-		}
+		// TODO: bounce
 	}
+	
+	static class CoolEntityBehavior implements EntityBehavior {
+		final long lastCollisionTime;
+		
+		public CoolEntityBehavior( long lastCollisionTime ) {
+			this.lastCollisionTime = lastCollisionTime;
+		}
+		
+		protected Entity withColorAndBehavior( Entity e, Color c, EntityBehavior b ) {
+			return new Entity(
+				e.tag, e.parent, e.time,
+				e.x, e.y, e.z, e.vx, e.vy, e.vz, e.ax, e.ay, e.az,
+				e.spaceRadius, e.solidRadius, e.mass, c, b
+			);
+		}
+		
+		@Override public Entity onMove( long time, Entity self,	EntityShell shell ) {
+			if( self.tag == "tag" ) {
+				System.err.println("Time since last collision "+(time - lastCollisionTime));
+			}
+			return (self.color == Color.RED && (time - lastCollisionTime >= 1000) ) ?
+				withColorAndBehavior( self, self.tag == "tag" ? Color.GREEN : Color.WHITE, this ) : self;
+		}
+		
+		@Override public Entity onCollision( long time, Entity self, Entity other, EntityShell shell ) {
+			assert self != null;
+			assert other != null;
+			if( self.tag == "tag" ) {
+				System.err.println("Collision!");
+			}
+			return withColorAndBehavior( self, self.tag == "tag" ? Color.ORANGE : Color.RED, new CoolEntityBehavior(time) );
+		}
+	};
 	
 	public static void main( String[] args ) throws Exception {
 		final Simulator sim = new Simulator();
-		for( int i=0; i<100; ++i ) {
-			Entity e = new Entity();
-			e.x = Math.random()*200-100;
-			e.y = Math.random()*200;
-			//e.z = Math.random()*200-100;
-			e.vx = Math.random()*20-10;
-			e.vy = Math.random()*200-10;
-			e.ay = -100;
-			e.solidRadius = 10;
-			e.color = Color.WHITE;
-			sim.space.addEntity( e );
-			e.behavior = new EntityBehavior() {
-				long lastCollision = Long.MIN_VALUE;
-				protected double damp( double val, double damp, double min ) {
-					val *= damp;
-					return Math.abs(val) < min ? 0 : val;
-				}
-				
-				@Override public void onMove(long time, Entity self) {
-					self.vx = damp( self.vx, 0.99, 1 );
-					self.vy = damp( self.vy, 0.99, 1 );
-					self.vz = damp( self.vz, 0.99, 1 );
-					//System.err.println(self.vy);
-					if( self.y < self.solidRadius*1.1 && Math.abs(self.vy) < 8 ) {
-						//System.err.println("Stopped");
-						self.vy = 0;
-						self.ay = 0;
-					}
-					if( self.y < self.solidRadius && self.vy < 0 ) {
-						// System.err.println("Bounce! " + self.x);
-						self.vy = -self.vy*0.8;
-					}
-					if( time - lastCollision > 1000 ) {
-						self.color = Color.WHITE;
-					}
-				}
-				@Override public void onCollision(long time, Entity self, Entity other) {
-					self.color = Color.RED;
-					lastCollision = time;
-				}
-			};
-		}
 		final QueuelessRealTimeEventSource<Object> eventSource = new QueuelessRealTimeEventSource<Object>();
+		
+		CoolEntityBehavior beh = new CoolEntityBehavior(0);
+		for( int i=0; i<100; ++i ) {
+			Entity e = new Entity(
+				i == 0 ? "tag" : null, null, eventSource.getCurrentTime(),
+				Math.random()*200-100, Math.random()*200, 0,
+				Math.random()*20-10, Math.random()*200-10, 0,
+				0, -100, 0,
+				10, 10,
+				1, i == 0 ? Color.GREEN : Color.WHITE, beh
+			);
+			sim.entityIndex.add(e);
+		}
 		
 		final Frame frame = new Frame();
 		final Canvas canv = new Canvas() {
@@ -275,7 +326,7 @@ public class Simulator extends BaseMutableAutoUpdatable<Object>
 				Graphics g = buf.getGraphics();
 				g.setColor(Color.BLACK);
 				g.fillRect(0, 0, getWidth(), getHeight());
-				for( Entity e : sim.space.allEntities ) {
+				for( Entity e : sim.entityIndex.allEntities ) {
 					g.setColor( e.color );
 					g.fillOval( (int)(e.x-e.solidRadius) + getWidth()/2, (int)(getHeight()-e.y-e.solidRadius), (int)(e.solidRadius*2), (int)(e.solidRadius*2) );
 				}
