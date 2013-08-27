92bc403f141ed8a5d13bc35ba772145dea92675f
TOGoS <togos00@gmail.com>
2013-08-26 21:02:36 -0500

    Remove Source/Sink classes because I'm not using them.

--- src/togos/networkrts/util/Sink.java
+++
@@ -1,5 +0,0 @@
-package togos.networkrts.util;
-
-public interface Sink<T, E extends Throwable> {
-	public void add(T t) throws E;
-}
--- src/togos/networkrts/util/Source.java
+++
@@ -1,5 +0,0 @@
-package togos.networkrts.util;
-
-public interface Source<T, E extends Throwable> {
-	public T remove() throws E;
-}
