bf7c4333b9d9ef03f8e1be9dda4d9e145425c55b
TOGoS <togos00@gmail.com>
2013-10-03 04:48:34 -0500

    Hovery selectey things.

--- SVGComponentArranger/thangs.js
+++ SVGComponentArranger/thangs.js
@@ -1,13 +1,29 @@
 (function() {
 	var components = document.getElementsByClassName('component');
 	var dragging = null;
+	var dragDistance = 0;
+	var selected = [];
+	
 	var dragX, dragY;
 	for( var c=0; c<components.length; ++c ) (function() {
-		var comp = components[c];
-		comp.addEventListener('mousedown', function(evt) {
-			dragging = comp;
+		var elem = components[c];
+		elem.addEventListener('mousedown', function(evt) {
+			dragging = elem;
 			dragX = evt.clientX;
 			dragY = evt.clientY;
+			dragDistance = 0;
+		});
+		elem.addEventListener('click', function(evt) {
+			if( dragDistance == 0 ) {
+				var si = selected.indexOf(elem);
+				if( si > -1 ) {
+					selected.splice(si, 1);
+					elem.className.baseVal = elem.className.baseVal.replace(/(?:^|\s)selected(?!\S)/g,'');
+				} else {
+					selected.push(elem);
+					elem.className.baseVal += ' selected';
+				}
+			}
 		});
 	})();
 	document.addEventListener('mousemove', function(evt) {
@@ -18,8 +34,12 @@
 			dragX = evt.clientX;
 			dragY = evt.clientY;
 		}
+		dragDistance += 1;
 	});
 	document.addEventListener('mouseup', function(evt) {
 		dragging = null;
+	});
+	document.addEventListener('keydown', function(evt) {
+		alert(evt.keyCode);
 	});
 })();
