5c9c0c04d90500a918c4b5a24f2646b98d6b1256
TOGoS <togos00@gmail.com>
2013-10-08 09:14:28 -0500

    Work towards getting wires to work in SVGComponentArranger.

--- SVGComponentArranger/thangs.js
+++ SVGComponentArranger/thangs.js
@@ -4,6 +4,26 @@
 		debugTextNode.textContent = text;
 	};
 	
+	var PortShape = function( name ) {
+		this.name = name;
+	}
+	PortShape.prototype.canConnectTo = function( otherGender ) { return false; }
+
+	var makePortShapePair = function(name) {
+		var male = new PortShape(name + " (male)");
+		var female = new PortShape(name + " (female)");
+		male.canConnectTo = function( other ) { return other == female; }
+		female.canConnectTo = function( other ) { return other == male; }
+		return {"male": male, "female": female};
+	};
+	
+	var portShapes = {
+		rj45: makePortShapePair("RJ45"),
+		barrel: {
+			dc2100um: makePortShapePair("2.1mm DC barrel")
+		}
+	};
+	
 	var LoadType = function( name ) {
 		this.name = name;
 	};
@@ -14,15 +34,23 @@
 		var name = loadTypeNames[i];
 		loadTypes[name] = new LoadType(name);
 	}
+
+	var eth   = loadTypes['ethernet'];
+	var pwr5v = loadTypes['power-5v'];
 	
-	var Port = function( id, loadType, x, y, dx, dy ) {
+	var Port = function( id, loadType, shape, x, y, dx, dy ) {
 		this.id = id;
 		this.loadType = loadType;
+		this.shape = shape;
 		this.x = x;
 		this.y = y;
 		this.dx = dx;
 		this.dy = dy;
 		this.loadType = loadType;
+		this.connectedTo = null;
+	};
+	Port.prototype.canConnectTo = function( other ) {
+		return this.shape.canConnectTo( other.shape );
 	};
 	
 	var Component = function( elem, ports ) {
@@ -30,20 +58,50 @@
 		this.elem = elem;
 		this.x = 0;
 		this.y = 0;
+		this.flexible = false;
 	};
 	Component.prototype.show = function() {
 		document.firstChild.appendChild( this.elem );
 	};
+	Component.prototype.recombobulate = function() {
+		var mtrx = this.elem.transform.baseVal.getItem(0).matrix;
+		mtrx.e = this.x;
+		mtrx.f = this.y;
+	};
 	Component.prototype.setPosition = function( x, y ) {
 		this.x = x;
 		this.y = y;
-		var mtrx = this.elem.transform.baseVal.getItem(0).matrix;
-		mtrx.e = x;
-		mtrx.f = y;
+		this.recombobulate();
 	};
 	
-	var eth   = loadTypes['ethernet'];
-	var pwr5v = loadTypes['power-5v'];
+	var Wire = function( path, loadType, portShape ) {
+		this.constructor( path, [
+			new Port('left' , loadType, portShape, -1, 0, -1, 0),
+			new Port('right', loadType, portShape, +1, 0, +1, 0)
+		]);
+		this.flexible = true;
+	};
+	Wire.prototype = new Component();
+	Wire.prototype.connect = function(end, otherPort, x, y) {
+		var p = this.ports[end];
+		if( p == null ) {
+			throw new Error("No such end '"+end+"' on wire!");
+		}
+		p.dx = -otherPort.dx;
+		p.dy = -otherPort.dy;
+		p.x = x;
+		p.y = y;
+		this.recombobulate();
+	};
+	Wire.prototype.recombobulate = function() {
+		var r = this.ports[0];
+		var l = this.ports[1];
+		this.path.d =
+			"M"+r.x+","+r.y+" "+
+			"C"+(r.x-r.dx*100)+","+(r.y-r.dy*100)+" "+
+			(l.x-l.dx*100)+","+(l.y-l.dy*100)+" "+
+			l.x+","+l.y;
+	};
 	
 	var makeComponentClass = function( templateElement, templatePorts ) {
 		var instantiatePorts = function( portList ) {
@@ -69,11 +127,11 @@
 	};
 		
 	var ZLES400 = makeComponentClass( document.getElementById('Z-LES-400'), [
-		new Port('e0', eth, -25,-25,-1, 0),
-		new Port('e1', eth, -25,+25,-1, 0),
-		new Port('e2', eth, +25,-25,+1, 0),
-		new Port('e3', eth, +25,+25,+1, 0),
-		new Port('pwr',pwr5v, 0,+50, 0,+1)
+		new Port('e0', eth, portShapes.rj45.female, -25,-25,-1, 0),
+		new Port('e1', eth, portShapes.rj45.female, -25,+25,-1, 0),
+		new Port('e2', eth, portShapes.rj45.female, +25,-25,+1, 0),
+		new Port('e3', eth, portShapes.rj45.female, +25,+25,+1, 0),
+		new Port('pwr',pwr5v, portShapes.barrel.dc2100um.male, 0,+50, 0,+1)
 	] );
 	
 	var dragging = null;
@@ -102,9 +160,17 @@
 		});
 	};
 	
-	var switch0 = new ZLES400(); 
+	var switch0, wire0;
+	
+	switch0 = new ZLES400(); 
 	switch0.setPosition( 300, 200 );
 	switch0.show();
+	
+	switch0 = new ZLES400(); 
+	switch0.setPosition( 200, 300 );
+	switch0.show();
+
+	//wire0 = new EthernetCable();	
 	
 	document.addEventListener('mousemove', function(evt) {
 		if( dragging != null ) {
