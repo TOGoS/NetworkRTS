2feaaeafd08f57e96dae0038679c6a7b4e8cb41c
TOGoS <togos00@gmail.com>
2014-03-11 10:48:13 -0500

    Mark 'Action' deprecated.

--- src/togos/networkrts/experimental/game19/world/Action.java
+++ src/togos/networkrts/experimental/game19/world/Action.java
@@ -1,6 +1,7 @@
 package togos.networkrts.experimental.game19.world;
 
 // TODO: Delete, replace with messages to the simulator
+@Deprecated
 public interface Action
 {
 	public void apply( ActionContext ctx );
