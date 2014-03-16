4cd05dacd13fed90e1b5b6b927015f68146e9827
TOGoS <togos00@gmail.com>
2014-03-15 22:30:33 -0500

    Move scene upload task out of JetManBehavior class.

---
+++ src/togos/networkrts/experimental/game19/world/msg/UploadSceneTask.java
@@ -0,0 +1,80 @@
+package togos.networkrts.experimental.game19.world.msg;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import togos.networkrts.experimental.game19.demo.ServerClientDemo.Scene;
+import togos.networkrts.experimental.game19.scene.Layer;
+import togos.networkrts.experimental.game19.scene.QuadTreeLayerData;
+import togos.networkrts.experimental.game19.scene.TileLayerData;
+import togos.networkrts.experimental.game19.scene.VisibilityChecker;
+import togos.networkrts.experimental.game19.scene.Layer.VisibilityClip;
+import togos.networkrts.experimental.game19.sim.AsyncTask;
+import togos.networkrts.experimental.game19.sim.UpdateContext;
+import togos.networkrts.experimental.game19.world.Message;
+import togos.networkrts.experimental.game19.world.NonTile;
+import togos.networkrts.experimental.game19.world.World;
+import togos.networkrts.experimental.game19.world.Message.MessageType;
+import togos.networkrts.experimental.game19.world.encoding.WorldConverter;
+import togos.networkrts.experimental.gameengine1.index.EntityRanges;
+import togos.networkrts.experimental.gameengine1.index.Visitor;
+
+public class UploadSceneTask implements AsyncTask {
+	// TODO: All sorts of stuff. Will probably want to
+	// make view distance to be a parameter,
+	// include various stats,
+	// wrap message in a few layers of framing
+	final NonTile viewer;
+	final World world;
+	final long uplinkBitAddress;
+	
+	public UploadSceneTask( NonTile viewer, World w, long uplinkBitAddress ) {
+		this.viewer = viewer;
+		this.world = w;
+		this.uplinkBitAddress = uplinkBitAddress;
+	}
+	
+	@Override
+	public void run( UpdateContext ctx ) {
+		int worldRadius = 1<<(world.rstSizePower-1);
+		double centerX = viewer.x, centerY = viewer.y;
+		
+		int intCenterX = (int)Math.floor(centerX);
+		int intCenterY = (int)Math.floor(centerY);
+		
+		int ldWidth = 41;
+		int ldHeight = 30;
+		// center of layer data
+		int ldCenterX = ldWidth/2;
+		int ldCenterY = ldHeight/2;
+		
+		// TODO: Only collect the ones actually visible
+		final List<NonTile> visibleNonTiles = new ArrayList<NonTile>();
+		
+		world.nonTiles.forEachEntity( EntityRanges.BOUNDLESS, new Visitor<NonTile>() {
+			@Override public void visit( NonTile v ) {
+				visibleNonTiles.add(v);
+			}
+		});
+		
+		// There are various ways to go about this:
+		// - do visibility checks, send only visible area
+		// - send nearby quadtree nodes
+		// - send entire world
+		
+		boolean sendTiles = true;
+		Layer l;
+		VisibilityClip visibilityClip = new Layer.VisibilityClip(centerX-(ldWidth-1)/2, centerY-(ldHeight-1)/2, centerX+(ldWidth-1)/2, centerY+(ldHeight-1)/2);
+		if( sendTiles ) {
+			TileLayerData layerData = new TileLayerData( ldWidth, ldHeight, 1 );
+			WorldConverter.nodeToLayerData( world.rst, -worldRadius, -worldRadius, 0, 1<<world.rstSizePower, layerData, intCenterX-ldCenterX, intCenterY-ldCenterY, ldWidth, ldHeight );
+			VisibilityChecker.calculateAndApplyVisibility(layerData, ldCenterX, ldCenterY, 0, 32);
+			l = new Layer( layerData, intCenterX-ldCenterX, intCenterY-ldCenterY, false, null, 0, 0, 0 );
+		} else {
+			int size = 1<<world.rstSizePower;
+			l = new Layer( new QuadTreeLayerData(world.rst, size), -size/2.0, -size/2.0, false, null, 0, 0, 0 );
+		}
+		Scene scene = new Scene( l, visibleNonTiles, centerX, centerY, visibilityClip );
+		ctx.sendMessage(Message.create(uplinkBitAddress, uplinkBitAddress, MessageType.INCOMING_PACKET, scene));
+	}
+}
--- src/togos/networkrts/experimental/game19/world/thing/jetman/JetManBehavior.java
+++ src/togos/networkrts/experimental/game19/world/thing/jetman/JetManBehavior.java
@@ -1,31 +1,18 @@
 package togos.networkrts.experimental.game19.world.thing.jetman;
 
-import java.util.ArrayList;
-import java.util.List;
 
 import togos.networkrts.experimental.game19.world.Message;
-import togos.networkrts.experimental.game19.demo.ServerClientDemo.Scene;
-import togos.networkrts.experimental.game19.scene.Layer;
-import togos.networkrts.experimental.game19.scene.Layer.VisibilityClip;
-import togos.networkrts.experimental.game19.scene.QuadTreeLayerData;
-import togos.networkrts.experimental.game19.scene.TileLayerData;
-import togos.networkrts.experimental.game19.scene.VisibilityChecker;
-import togos.networkrts.experimental.game19.sim.AsyncTask;
 import togos.networkrts.experimental.game19.sim.NonTileUpdateContext;
-import togos.networkrts.experimental.game19.sim.UpdateContext;
 import togos.networkrts.experimental.game19.world.BitAddresses;
 import togos.networkrts.experimental.game19.world.BlockStack;
-import togos.networkrts.experimental.game19.world.Message.MessageType;
 import togos.networkrts.experimental.game19.world.MessageSet;
 import togos.networkrts.experimental.game19.world.NonTile;
 import togos.networkrts.experimental.game19.world.NonTile.Icon;
+import togos.networkrts.experimental.game19.world.msg.UploadSceneTask;
 import togos.networkrts.experimental.game19.world.NonTileBehavior;
 import togos.networkrts.experimental.game19.world.RSTNode;
 import togos.networkrts.experimental.game19.world.World;
-import togos.networkrts.experimental.game19.world.encoding.WorldConverter;
 import togos.networkrts.experimental.gameengine1.index.AABB;
-import togos.networkrts.experimental.gameengine1.index.EntityRanges;
-import togos.networkrts.experimental.gameengine1.index.Visitor;
 import togos.networkrts.util.BitAddressUtil;
 
 public class JetManBehavior implements NonTileBehavior {
@@ -105,50 +92,7 @@ public class JetManBehavior implements NonTileBehavior {
 	@Override public NonTile update(final NonTile nt, long time, final World world,
 		MessageSet messages, NonTileUpdateContext updateContext
 	) {
-		updateContext.startAsyncTask( new AsyncTask() {
-			@Override public void run( UpdateContext ctx ) {
-				int worldRadius = 1<<(world.rstSizePower-1);
-				double centerX = nt.x, centerY = nt.y;
-				
-				int intCenterX = (int)Math.floor(centerX);
-				int intCenterY = (int)Math.floor(centerY);
-				
-				int ldWidth = 41;
-				int ldHeight = 30;
-				// center of layer data
-				int ldCenterX = ldWidth/2;
-				int ldCenterY = ldHeight/2;
-				
-				// TODO: Only collect the ones actually visible
-				final List<NonTile> visibleNonTiles = new ArrayList<NonTile>();
-				
-				world.nonTiles.forEachEntity( EntityRanges.BOUNDLESS, new Visitor<NonTile>() {
-					@Override public void visit( NonTile v ) {
-						visibleNonTiles.add(v);
-					}
-				});
-				
-				// There are various ways to go about this:
-				// - do visibility checks, send only visible area
-				// - send nearby quadtree nodes
-				// - send entire world
-				
-				boolean sendTiles = true;
-				Layer l;
-				VisibilityClip visibilityClip = new Layer.VisibilityClip(centerX-(ldWidth-1)/2, centerY-(ldHeight-1)/2, centerX+(ldWidth-1)/2, centerY+(ldHeight-1)/2);
-				if( sendTiles ) {
-					TileLayerData layerData = new TileLayerData( ldWidth, ldHeight, 1 );
-					WorldConverter.nodeToLayerData( world.rst, -worldRadius, -worldRadius, 0, 1<<world.rstSizePower, layerData, intCenterX-ldCenterX, intCenterY-ldCenterY, ldWidth, ldHeight );
-					VisibilityChecker.calculateAndApplyVisibility(layerData, ldCenterX, ldCenterY, 0, 32);
-					l = new Layer( layerData, intCenterX-ldCenterX, intCenterY-ldCenterY, false, null, 0, 0, 0 );
-				} else {
-					int size = 1<<world.rstSizePower;
-					l = new Layer( new QuadTreeLayerData(world.rst, size), -size/2.0, -size/2.0, false, null, 0, 0, 0 );
-				}
-				Scene scene = new Scene( l, visibleNonTiles, centerX, centerY, visibilityClip );
-				ctx.sendMessage(Message.create(clientBitAddress, clientBitAddress, MessageType.INCOMING_PACKET, scene));
-			}
-		});
+		updateContext.startAsyncTask(new UploadSceneTask(nt, world, clientBitAddress));
 		
 		double newX = nt.x, newY = nt.y;
 		double newVx = nt.vx, newVy = nt.vy;
