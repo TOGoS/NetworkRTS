7b05b93524d56b3b4c74fc552450476fb6045e75
TOGoS <togos00@gmail.com>
2014-04-11 16:22:57 -0500

    Add EtherTypes class, referenced by a few commits ago.  Whups.

---
+++ src/togos/networkrts/experimental/packet19/EtherTypes.java
@@ -0,0 +1,10 @@
+package togos.networkrts.experimental.packet19;
+
+public class EtherTypes
+{
+	private EtherTypes() { }
+	
+	public static final short IP4 = (short)0x0800;
+	public static final short ARP = (short)0x0806;
+	public static final short IP6 = (short)0x86DD;
+}
