12950d92d5f7178c1b8291bb26899b9e23ed05c7
TOGoS <togos00@gmail.com>
2014-04-15 12:09:29 -0500

    Turn WorldSaveDemo into a unit test (which currently fails because of some problem I don't yet understand).

--- test/togos/networkrts/experimental/game19/io/WorldCoolCerealizationTest.java
+++ test/togos/networkrts/experimental/game19/io/WorldCoolCerealizationTest.java
@@ -1,29 +1,34 @@
-package togos.networkrts.experimental.game19.demo;
+package togos.networkrts.experimental.game19.io;
 
 import java.io.File;
 
+import junit.framework.TestCase;
 import togos.networkrts.experimental.game19.ResourceContext;
-import togos.networkrts.experimental.game19.io.CerealWorldIO;
+import togos.networkrts.experimental.game19.demo.DemoWorld;
 import togos.networkrts.experimental.game19.world.World;
 import togos.networkrts.util.HasURI;
 
-public class WorldSaveDemo
+public class WorldCoolCerealizationTest extends TestCase
 {
-	public static void main(String[] args) throws Exception {
+	public void testEncodeDecode() throws Exception {
 		ResourceContext rc = new ResourceContext(new File(".ccouch"));
+		
 		HasURI worldRef;
 		{
 			CerealWorldIO worldIo = new CerealWorldIO(rc.getByteArrayRepository());
+			
+			// TODO: build a simpler world so this test doesn't take so long
 			World w = DemoWorld.initWorld(rc);
+			
 			worldRef = worldIo.storeObject(w);
-			System.err.println("Saved: "+worldRef);
+			assertNotNull(worldRef);
 		}
 		
 		{
 			// Now let's try to load it again!
 			CerealWorldIO worldLoader = new CerealWorldIO(rc.getByteArrayRepository());
 			World w = worldLoader.getObject(worldRef, World.class);
-			System.err.println("Loaded: "+w);
+			assertNotNull(w);
 		}
 	}
 }
