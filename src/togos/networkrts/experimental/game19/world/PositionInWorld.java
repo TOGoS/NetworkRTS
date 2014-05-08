315006c1fc8ef154032030109f7d65fa6ec24be6
TOGoS <togos00@gmail.com>
2014-05-08 17:59:12 -0500

    Commit PositionInWorld class, needed since several commits ago.

---
+++ src/togos/networkrts/experimental/game19/world/PositionInWorld.java
@@ -0,0 +1,12 @@
+package togos.networkrts.experimental.game19.world;
+
+public class PositionInWorld
+{
+	public final double x, y, z;
+	
+	public PositionInWorld( double x, double y, double z ) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+}
