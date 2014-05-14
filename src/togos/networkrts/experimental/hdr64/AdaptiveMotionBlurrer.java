c93e4e70c4bc4bb3a96c8b6cdd5124e86926a82a
TOGoS <togos00@gmail.com>
2014-05-14 09:30:10 -0500

    AMB adjustments.

--- src/togos/networkrts/experimental/hdr64/AdaptiveMotionBlurrer.java
+++ src/togos/networkrts/experimental/hdr64/AdaptiveMotionBlurrer.java
@@ -22,18 +22,25 @@ public class AdaptiveMotionBlurrer
 		public void render( double t, HDR64Buffer into );
 	}
 	
-	double[] ats = new double[] { 0.0, 0.6, 0.3, 0.9 };
+	double[] ats = new double[] { 0.5, 0.0, 1.0, 0.3, 0.7, 0.15, 0.85, 0.4, 0.6 };
 	
 	public int render( Renderer renderer, long tryToFinishBy ) {
 		Random r = new Random();
 		
 		// Draw the first one directly onto the accumulation buffer to save some time
+		long preRender = timeKeeper.currentTime();
 		renderer.render(ats[0], accumulationBuffer);
+		long postRender = timeKeeper.currentTime();
 		int iterations = 1;
 		
-		while( timeKeeper.currentTime() < tryToFinishBy ) {
+		long renderTime = postRender - preRender;
+		
+		while( timeKeeper.currentTime() < tryToFinishBy-renderTime ) {
+			preRender = timeKeeper.currentTime();
 			renderer.render(iterations >= ats.length ? r.nextDouble() : ats[iterations], drawBuffer);
 			HDR64Util.add(drawBuffer.data, accumulationBuffer.data);
+			postRender = timeKeeper.currentTime();
+			renderTime = ( renderTime + (postRender - preRender) ) / 2;
 			++iterations;
 		} 
 		
