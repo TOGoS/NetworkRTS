88c28e75af0edcc8c4fc1453b85938e2bd8b016f
TOGoS <togos00@gmail.com>
2014-06-01 03:39:12 -0500

    Fix HDR64MonteCarloScaler.

--- src/togos/networkrts/experimental/hdr64/HDR64MonteCarloScaler.java
+++ src/togos/networkrts/experimental/hdr64/HDR64MonteCarloScaler.java
@@ -4,22 +4,49 @@ import java.util.Random;
 
 public class HDR64MonteCarloScaler
 {
-	final Random rand = new Random();
+	protected final int samplePointCount = 32; 
+	
+	float[] samplePoints = new float[samplePointCount*2];
+	
+	public HDR64MonteCarloScaler() {
+		final Random rand = new Random();
+		for( int i=0; i<samplePoints.length; ++i ) {
+			samplePoints[i] = rand.nextFloat();
+		}
+	}
 	
 	public HDR64Buffer scale( HDR64Buffer in, int w, int h ) {
 		// TODO: Flip instead of ignoring negatives
-		if( w < 0 ) w = -w;
+		int sx0, sx1, sy0, sy1;
+		
+		if( w < 0 ) {
+			sx0 = in.width;
+			sx1 = -1;
+			w = -w;
+		} else {
+			sx0 = 0;
+			sx1 = in.width;
+		}
+		if( h < 0 ) {
+			sy0 = in.height;
+			sy1 = -1;
+			h = -h;
+		} else {
+			sy0 = 0;
+			sy1 = in.height;
+		}
+		
 		if( h < 0 ) h = -h;
 		
 		HDR64Buffer out = new HDR64Buffer(w, h);
-		int spp = 10;
+		int spp = samplePointCount;
 		for( int y=0; y<h; ++y ) {
 			for( int x=0; x<w; ++x ) {
 				int sampleCount = 0;
 				long v = 0;
-				for( int i=0; i<spp; ++i ) {
-					int ix = (int)((x + rand.nextDouble()) * in.width / w);
-					int iy = (int)((y + rand.nextDouble()) * in.height / h);
+				for( int i=spp*2-1; i>=0; ) {
+					int ix = (int)(sx0 + (x + samplePoints[i--]) * (sx1-sx0) / w);
+					int iy = (int)(sy0 + (y + samplePoints[i--]) * (sy1-sy0) / h);
 					long sample = in.data[ix + iy*in.width];
 					if( (sample & HDR64Util.HDR_ALPHA_MASK) != 0 ) {
 						++sampleCount;
